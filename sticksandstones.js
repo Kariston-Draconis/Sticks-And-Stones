da1d538e5db49d4d125610e1eb4e3f1187f5057d
Alex May <thrawnrulz@hotmail.com>
2017-06-14 12:01:06 +1000

    I can make images appear now

--- sticksandstones.js
+++ sticksandstones.js
@@ -1,3 +1,20 @@
 console.log('Sticks And Stones');
 
 var count = 0;
+
+var board = document.querySelector('.board')
+var square = document.querySelector('.gridSquare')
+var stone = document.querySelector('.stone')
+var bones = document.querySelector('.bones')
+
+var play = function() {
+  if(count % 2 == 0) {
+    stone.style.visibility = 'visible';
+    count = count + 1;
+  } else {
+    bones.style.visibility = 'visible';
+    count = count + 1;
+  }
+}
+
+board.addEventListener('click', play)
